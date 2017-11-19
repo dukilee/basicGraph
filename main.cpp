5cf44f3214a5fc4e63fde63b83502b98010c8580
dukilee <lekdekum@gmail.com>
2017-11-19 18:34:02 -0200

    Spliting in modules

--- main.cpp
+++ main.cpp
@@ -1,20 +1,12 @@
 #include <bits/stdc++.h>
+#include "includes/DFS.h"
+#include "includes/Dijikstra.h"
+#include "includes/LineType.h"
+#include "includes/Prim.h"
 
 using namespace std;
 
-enum{
-	IS_EDGE,
-	IS_FUNCTION,
-	IS_NOTHING,
-	IS_WHITELINE
-};
-
-
-struct Line{
-	string orig, dest;
-	char op;
-	int val;
-};
+bool bidirect = false;
 
 double stringToDouble(string s){
 	double val = 0;
@@ -35,125 +27,6 @@ double stringToDouble(string s){
 		val = val + decimal;
 	}
 	return val;
-}
-
-bool DFS(map<string, vector<pair<string, double> > >& graph, map<string, bool>& flag, string curr, string finish){
-	if(curr == finish) return true;
-	if(flag[curr]) return false;
-	flag[curr] = true;
-	for(vector<pair<string, double> >::iterator it = graph[curr].begin(); it!=graph[curr].end(); it++){
-		if(DFS(graph, flag, it->first, finish)) return true;
-	}
-	return false;
-}
-
-bool DFS(map<string, vector<pair<string, double> > >& graph, string start, string finish){
-	map<string, bool> flag;
-	return DFS(graph, flag, start, finish);
-}
-
-double dijikstra(map<string, vector<pair<string, double> > >& graph, string start, string finish){
-
-
-	priority_queue<pair<double, string>, 
-					vector< pair<double, string> >,
-					greater< pair<double, string> > > pq;
-
-	map<string, double> dist;
-
-	dist[start] = 0;
-	pq.push(make_pair(0, start));
-	while(!pq.empty()){
-		double z = pq.top().first;
-		string x = pq.top().second;
-		pq.pop();
-		if(dist[x]!=z){
-			continue;
-		}
-
-		for(vector<pair<string, double> >::iterator it = graph[x].begin(); it!=graph[x].end(); it++){
-			if(dist.find(it->first)==dist.end() || dist[it->first]>z + it->second){
-				dist[it->first] = z + it->second;
-				pq.push(make_pair(dist[it->first], it->first));
-			}
-		}
-
-	}
-	if(dist.find(finish)==dist.end()) return -1;//not connected
-	return dist[finish];
-}
-
-
-
-int decideLineType(string currLine){
-	int openCount = 0;
-	bool thereIsBracket = false;
-	bool edgeDefinition = false;
-	//check if is an empty line
-
-	bool isEmptyLine = true;
-	for(int i = 0; i<currLine.size(); i++){
-		if(currLine[i]!=' ' && currLine[i]!='\n' && currLine[i]!='\t'){
-			isEmptyLine = false;
-		}
-	}
-	if(isEmptyLine)
-		return IS_WHITELINE;
-
-
-	for(int i = 0; i<currLine.size(); i++){
-		if(currLine[i]=='('){
-			thereIsBracket = true;
-			openCount++;
-		}else if(currLine[i]==')'){
-			openCount--;
-			if(openCount<0){
-				std::cout<<"There are too many close brackets"<<std::endl;
-				return IS_NOTHING;
-			}
-		}else if(currLine[i]=='-'){
-			if(thereIsBracket){
-				std::cout<<"There is a bracket edge definition, something is strange\n";
-				return IS_NOTHING;
-			}
-
-			i++;
-			if(i<currLine.size() && currLine[i]=='>'){
-				edgeDefinition = true;
-			}else{
-				std::cout<<"Error in edge definition\n";
-				return IS_NOTHING;
-			}
-		}else if(!isalnum(currLine[i]) && currLine[i]!=' ' &&
-					currLine[i]!='\n' && currLine[i]!='\t' && currLine[i]!='.'){
-			if(currLine[i]==','){
-				if(openCount==0){
-					cout<<"There is \',\' outside function call\n";
-					return IS_NOTHING;
-				}
-			}else{
-				std::cout<<"There is a non-alfaNumeric character \'"<<currLine[i]<<"\'"<<std::endl;
-				return IS_NOTHING;
-			}
-		}
-	}
-	if(openCount>0){
-		std::cout<<"There are too few bracket closers\n";
-		return IS_NOTHING;
-	}
-	if(edgeDefinition){
-		if(thereIsBracket){
-			cout<<"There are brackets and edge definition\n";
-			return IS_NOTHING;
-		}else{
-			return IS_EDGE;
-		}
-	}
-	if(thereIsBracket)
-		return IS_FUNCTION;
-
-	return IS_EDGE;
-
 }
 
 bool addEdge(string currLine, std::map<std::string, std::vector<std::pair<std::string, double> > >& graph){
@@ -195,8 +68,13 @@ bool addEdge(string currLine, std::map<std::string, std::vector<std::pair<std::s
 	}
 
 	double weightVal = stringToDouble(weight);
-
 	graph[start].push_back(make_pair(finish, weightVal));
+	if(bidirect){
+		graph[finish].push_back(make_pair(start, weightVal));
+		cout<<"New edge: "<<start<<" -> "<<finish<<" (weight "<<weightVal<<") BID\n";	
+	}else{
+		cout<<"New edge: "<<start<<" -> "<<finish<<" (weight "<<weightVal<<") UNI\n";
+	}
 	return true;
 
 }
@@ -229,7 +107,7 @@ bool resolveFunction(string currLine, std::map<std::string, std::vector<std::pai
 			cout<<"Check params for DFS call\n";
 			return false;
 		}
-		if(DFS(graph, params[0], params[1])){
+		if(DFS::run(graph, params[0], params[1])){
 			cout<<"\'"<<params[0]<<"\' and \'"<<params[1]<<"\' are conenected\n";
 		}else{
 			cout<<"\'"<<params[0]<<"\' and \'"<<params[1]<<"\' are NOT conenected\n";
@@ -239,8 +117,34 @@ bool resolveFunction(string currLine, std::map<std::string, std::vector<std::pai
 			cout<<"Check params for distance call\n";
 			return false;	
 		}
-		double dist = dijikstra(graph, params[0], params[1]);
+		double dist = Dijikstra::run(graph, params[0], params[1]);
 		cout<<"Distance \'"<<params[0]<<"\' and \'"<<params[1]<<"\' equals to "<<dist<<"\n";		
+	}else if(functionName == "mst"){
+		if(params.size()!=0){
+			cout<<"Check params for mst call\n";
+			return false;	
+		}
+		if(!bidirect){
+			cout<<"WARNING, the graph is unidirect\n";
+		}
+		double dist = Prim::run(graph);
+		cout<<"The cost of the Minimum Spanning Tree equals to "<<dist<<"\n";		
+	
+	}else if(functionName == "setBidirect"){
+		if(params.size()!=0){
+			cout<<"Check params for setBidirect call\n";
+			return false;	
+		}
+		bidirect = true;
+		cout<<"Considering bidirect\n";
+	}else if(functionName == "setUnidirect"){
+		if(params.size()!=0){
+			cout<<"Check params for setUnidirect call\n";
+			return false;	
+		}
+		bidirect = false;
+		cout<<"Considering unidirect\n";
+
 	}else{
 		cout<<"Unknown function\n";
 		return false;
@@ -250,24 +154,29 @@ bool resolveFunction(string currLine, std::map<std::string, std::vector<std::pai
 
 
 int main(){
+	bidirect =false;
 	std::map<std::string, std::vector<std::pair<std::string, double> > > graph;
 
 	string currLine;
-	int lineCount = 0;
+	int lineCount = 1;
+	cout<<lineCount<<"> ";
 	while(getline(cin, currLine)){
-		lineCount++;
-		int lineType = decideLineType(currLine);
+		LineTypeId lineType = LineType::decideLineType(currLine);
 		
-		if(lineType == IS_NOTHING){
+		if(lineType == LineTypeId::IS_NOTHING){
 			cout<<"Line "<<currLine<<": \'"<<currLine<<"\'\n";
-		}else if(lineType==IS_EDGE){
+		}else if(lineType==LineTypeId::IS_EDGE){
 			if(!addEdge(currLine, graph)){
 				cout<<lineCount<<": \'"<<currLine<<"\'\n";
-			}
-		}else if(lineType==IS_FUNCTION){
+			}else
+				lineCount++;
+		}else if(lineType==LineTypeId::IS_FUNCTION){
 			if(!resolveFunction(currLine, graph)){
 				cout<<lineCount<<": \'"<<currLine<<"\'\n";				
-			}
+			}else
+				lineCount++;
 		}
+		cout<<lineCount<<"> ";
 	}
+	cout<<endl;
 }
